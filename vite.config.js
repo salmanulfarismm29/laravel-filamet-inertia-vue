debe0bf17913c5d49e874768d78a62c103b0ca5b
salmanulfarismm29 <salmanul20001128@gmail.com>
2026-01-14 19:53:12 +0530

    add vue and viteImagemin in vite config

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,31 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'vite'
+import laravel from 'laravel-vite-plugin'
+import vue from '@vitejs/plugin-vue'
+import tailwindcss from '@tailwindcss/vite'
+import viteImagemin from 'vite-plugin-imagemin'
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-        tailwindcss(),
-    ],
-    server: {
-        watch: {
-            ignored: ['**/storage/framework/views/**'],
-        },
+  plugins: [
+    laravel({
+      input: [
+        'resources/css/app.css',
+        'resources/js/app.js',
+      ],
+      refresh: true,
+    }),
+    vue(),
+    tailwindcss(),
+    viteImagemin({
+      gifsicle: { optimizationLevel: 7 },
+      optipng: { optimizationLevel: 7 },
+      mozjpeg: { quality: 75 },
+      pngquant: { quality: [0.7, 0.85] },
+      svgo: true,
+    }),
+  ],
+  server: {
+    watch: {
+      ignored: ['**/storage/framework/views/**'],
     },
-});
+  },
+})
