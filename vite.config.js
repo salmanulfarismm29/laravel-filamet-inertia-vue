72ef6f4ebfac8bb85139283cada27c98af5e29df
salmanulfarismm29 <salmanul20001128@gmail.com>
2026-01-14 19:51:21 +0530

    add react package

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,25 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'vite'
+import laravel from 'laravel-vite-plugin'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+import viteImagemin from 'vite-plugin-imagemin'
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-        tailwindcss(),
-    ],
-    server: {
-        watch: {
-            ignored: ['**/storage/framework/views/**'],
-        },
+  plugins: [
+    laravel({
+      input: [
+        'resources/css/app.css',
+        'resources/js/app.jsx',
+      ],
+      refresh: true,
+    }),
+    react(),
+    tailwindcss(),
+    viteImagemin(),
+  ],
+  server: {
+    watch: {
+      ignored: ['**/storage/framework/views/**'],
     },
-});
+  },
+})
